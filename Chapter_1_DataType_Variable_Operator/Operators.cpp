f74a567a2bfed47b498f3ae1dcdc46877c03cc7d
rohitwithcode <radharani7253@gmail.com>
2023-09-26 21:04:56 +0530

    Variable Operators files added

---
+++ Chapter_1_DataType_Variable_Operator/Operators.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=3/6;
+    cout<<a<<endl;
+
+    int a1=3.0/6;
+    cout<<a1<<endl;
+
+    cout<<3.0/6<<endl;
+
+    int x=4;
+    int y=6;
+    
+    bool b=(x==y);
+    cout<<b<<endl;
+
+    bool b1=(x>=y);
+    cout<<b1<<endl;
+
+    bool b2=(x<=y);
+    cout<<b2<<endl;
+
+    bool b3=(x!=y);
+    cout<<b3<<endl;
+    return 0;
+}
