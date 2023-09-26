d1df5c3d39a0e3a21d423d807aa73a5193cd66ec
rohitwithcode <radharani7253@gmail.com>
2023-09-26 19:47:34 +0530

    DataType variable Added

---
+++ DataType_Variable.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=34;
+    cout<<"A = "<<endl;
+
+    char ch='R';
+    cout<<"Ch = "<<ch<<endl;
+
+    bool bl=true;
+    cout<<"bl = "<<bl<<endl;
+
+    float fl=4.4;
+    cout<<"fl = "<<fl<<endl;
+
+    double d=5.64545;
+    cout<<"d = "<<d<<endl;
+
+    int size=sizeof(a);
+    cout<<"Size of the a is "<<size<<endl;
+    return 0;
+}
