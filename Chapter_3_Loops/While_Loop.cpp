75a95d8a8b7c783ac4dac7b1138435bfa6d38cda
rohitwithcode <radharani7253@gmail.com>
2023-09-27 15:38:35 +0530

    While Loop added

---
+++ Chapter_3_Loops/While_Loop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n;
+    cout<<"Enter the value = ";
+    cin>>n;
+
+    int i=1;
+
+    while (i<=n){
+        cout<<i<<" ";
+        i++;
+    }
+    
+return 0;
+}
