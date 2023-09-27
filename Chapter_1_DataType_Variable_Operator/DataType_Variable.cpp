508874224a35521f6882b05ee865effda062d0ce
rohitwithcode <radharani7253@gmail.com>
2023-09-27 13:53:34 +0530

    DataType_Variable add

---
+++ Chapter_1_DataType_Variable_Operator/DataType_Variable.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+/*    
+    int a=34;
+    cout<<"A = "<<endl;
+
+    char ch='R';
+    cout<<"Ch = "<<ch<<endl;
+
+    bool bl=true;
+    cout<<"bl = "<<bl<<endl;
+
+    float fl=4.4;
+    cout<<"fl = "<<fl<<endl;
+
+    double d=5.64545;
+    cout<<"d = "<<d<<endl;
+
+    int size=sizeof(a);
+    cout<<"Size of the a is "<<size<<endl;
+*/
+
+    int a='a';
+    cout<<a<<endl;
+
+    char ch=98;
+    cout<<ch<<endl;
+
+    char c=123456;
+    cout<<c<<endl;
+
+
+    // unsigned int x=-345;
+    // cout<<x<<endl;
+
+
+    return 0;
+}
