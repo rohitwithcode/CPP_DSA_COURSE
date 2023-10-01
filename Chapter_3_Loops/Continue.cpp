3fa5b541de0cee66e39277638a60b9dfcfb4edb8
rohitwithcode <radharani7253@gmail.com>
2023-10-01 15:48:37 +0530

    Continue added

---
+++ Chapter_3_Loops/Continue.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number =";
+    cin>>n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout<<"Hi Rohit"<<endl;
+        cout<<"Hey Rohit"<<endl;
+        continue;
+        cout<<"Please Reply me"<<endl;
+    }
+    
+return 0;
+}
