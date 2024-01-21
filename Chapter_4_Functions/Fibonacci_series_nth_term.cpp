a8632e14682258e38d2ee6e3e17b1368da314629
rohitwithcode <radharani7253@gmail.com>
2024-01-21 19:52:40 +0530

    fibonacci sequence file added

---
+++ Chapter_4_Functions/Fibonacci_series_nth_term.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int fib(int n){
+
+    int firstTerm=0;
+    int secondTerm=1;
+    int nextTerm=0;
+    for(int i=3;i<=n;i++){
+         nextTerm=firstTerm+secondTerm;
+
+        firstTerm=secondTerm;
+        secondTerm=nextTerm;
+    }
+    return nextTerm;
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the value of the n : ";
+    cin>>n;
+
+    cout<<"The "<<n<<" term of the fibonacci sequence is "<<fib(n);
+    return 0;
+}
