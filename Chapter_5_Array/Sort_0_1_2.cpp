f269b7256e2b3058c0b468678f37cab30c1651c6
rohitwithcode <radharani7253@gmail.com>
2024-01-25 16:50:28 +0530

    Sort 0,1 and 2s file added

---
+++ Chapter_5_Array/Sort_0_1_2.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+void sort_0_1_2(int arr[],int size)
+{
+   int low=0;
+   int mid=0;
+   int high=size-1;
+
+   while (mid<high)
+   {
+        if (arr[low]==0 || arr[mid]==0)
+        {
+            int temp=arr[low];
+            arr[low]=arr[mid];
+            arr[mid]=temp;
+            low++;
+            mid++;
+        }
+        else if (arr[mid]==2)
+        {
+            int temp=arr[mid];
+            arr[mid]=arr[high];
+            arr[high]=temp;
+            high--;
+        }
+        else{
+            mid++;
+        }   
+   }
+   for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }  
+}
+int main()
+{
+    int arr[]={2,2,2,1,0,2,2,1,0,1,1,0,2};
+
+    sort_0_1_2(arr,13);
+    return 0;
+}
