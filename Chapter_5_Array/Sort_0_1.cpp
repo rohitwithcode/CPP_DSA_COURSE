b38bfdcd978c722775d255f989547d719497600f
rohitwithcode <radharani7253@gmail.com>
2024-01-25 15:22:43 +0530

    Sort 0 and 2 file added

--- Chapter_5_Array/Sort_0_1.cpp
+++ Chapter_5_Array/Sort_0_1.cpp
@@ -6,18 +6,34 @@ void sort_0_1(int arr[],int size)
     int left=0,right=size-1;
    while (left<right)
    {
-    if (arr[left]==0 && arr[right]==1)
+    // if (arr[left]==0 && arr[right]==1)
+    // {
+    //     left++;
+    //     right--;
+    // }
+    // else if (arr[left]==1 && arr[right]==1)
+    // {
+    //     right--;
+    // }
+    // else if (arr[left]==0 && arr[right]==0)
+    // {
+    //     left++;
+    // }
+    // else{
+    //     int temp=arr[left];
+    //     arr[left]=arr[right];
+    //     arr[right]=temp;
+    //     left++;
+    //     right--;
+    // } 
+
+    if (arr[left]==0)
     {
         left++;
-        right--;
     }
-    else if (arr[left]==1 && arr[right]==1)
+    else if (arr[right]==1)
     {
         right--;
-    }
-    else if (arr[left]==0 && arr[right]==0)
-    {
-        left++;
     }
     else{
         int temp=arr[left];
@@ -26,12 +42,12 @@ void sort_0_1(int arr[],int size)
         left++;
         right--;
     }
+    
    }
    for (int i = 0; i < size; i++)
     {
         cout<<arr[i]<<" ";
     }
-   
 }
 
  int main()
