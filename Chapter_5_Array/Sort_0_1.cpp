760f81bf956a8c30b8134c181cd0015caa6df92d
rohitwithcode <radharani7253@gmail.com>
2024-01-25 15:43:51 +0530

    Sort 0 and 1 file added

--- Chapter_5_Array/Sort_0_1.cpp
+++ Chapter_5_Array/Sort_0_1.cpp
@@ -6,6 +6,29 @@ void sort_0_1(int arr[],int size)
     int left=0,right=size-1;
    while (left<right)
    {
+     if (arr[left]==0)
+    {
+        left++;
+    }
+    else if (arr[right]==1)
+    {
+        right--;
+    }
+    else{
+        int temp=arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
+        left++;
+        right--;
+    }
+    
+   }
+   for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+
     // if (arr[left]==0 && arr[right]==1)
     // {
     //     left++;
@@ -27,32 +50,32 @@ void sort_0_1(int arr[],int size)
     //     right--;
     // } 
 
-    if (arr[left]==0)
-    {
-        left++;
-    }
-    else if (arr[right]==1)
-    {
-        right--;
-    }
-    else{
-        int temp=arr[left];
-        arr[left]=arr[right];
-        arr[right]=temp;
-        left++;
-        right--;
-    }
+   
+//     while (arr[left]==0 && left<right)
+//     {
+//         left++;
+//     }
+//     while (arr[right]==1 && left<right)
+//     {
+//         right--;
+//     }
     
-   }
-   for (int i = 0; i < size; i++)
-    {
-        cout<<arr[i]<<" ";
-    }
+//     int temp=arr[left];
+//     arr[left]=arr[right];
+//     arr[right]=temp;
+//     left++;
+//     right--;
+    
+//    }
+//    for (int i = 0; i < size; i++)
+//     {
+//         cout<<arr[i]<<" ";
+//     }
 }
 
  int main()
  {
-    int arr[]={1,0,1,0,1,1,0};
-    sort_0_1(arr,7);
+    int arr[]={1,0,1,0,1,1,0,1,1,0,1};
+    sort_0_1(arr,11);
     return 0;
  }
