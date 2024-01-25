d474780249c23b3d810612fbd82282a2c522e6d7
rohitwithcode <radharani7253@gmail.com>
2024-01-25 14:45:28 +0530

    Sort 0 and 1 file added

---
+++ Chapter_5_Array/Sort_0_1.cpp
@@ -0,0 +1,42 @@
+#include<iostream> 
+using namespace std;
+
+void sort_0_1(int arr[],int size)
+{
+    int left=0,right=size-1;
+   while (left<right)
+   {
+    if (arr[left]==0 && arr[right]==1)
+    {
+        left++;
+        right--;
+    }
+    else if (arr[left]==1 && arr[right]==1)
+    {
+        right--;
+    }
+    else if (arr[left]==0 && arr[right]==0)
+    {
+        left++;
+    }
+    else{
+        int temp=arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
+        left++;
+        right--;
+    }
+   }
+   for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+   
+}
+
+ int main()
+ {
+    int arr[]={1,0,1,0,1,1,0};
+    sort_0_1(arr,7);
+    return 0;
+ }
