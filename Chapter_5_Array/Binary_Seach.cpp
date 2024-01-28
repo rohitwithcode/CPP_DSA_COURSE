b49f4ce5fdae75bb4fbc7f83b743db4f8a096f03
rohitwithcode <radharani7253@gmail.com>
2024-01-28 20:46:27 +0530

    Binary Search file added

--- Chapter_5_Array/Binary_Seach.cpp
+++ Chapter_5_Array/Binary_Seach.cpp
@@ -5,8 +5,9 @@ int binarySearch(int arr[],int size,int element)
 {
     int low=0;
     int high=size-1;
-    int mid=(low+high)/2;
+    // int mid=(low+high)/2;
 
+    int mid=low + (high-low)/2;
     
     while (low <= high)
     {
@@ -19,11 +20,13 @@ int binarySearch(int arr[],int size,int element)
         {
             low=mid+1;
         }
-        else
+        else // element < arr[mid]
         {
             high=mid-1;
         }
-        mid=(low+high)/2;
+        // mid=(low+high)/2;
+
+         mid=low + (high-low)/2;
     }
     return -1;
 }
@@ -33,10 +36,11 @@ int main()
     int arrEven[]={2,4,6,8,10,12};
     int arrOdd[]={1,3,5,7,9};
 
-    int element=binarySearch(arrEven,6,8);
-    cout<<"The index of 8 is "<<element<<endl;
+    int element=binarySearch(arrEven,6,10);
+    cout<<"The index of 10 is "<<element<<endl;
 
-    int index=binarySearch(arrOdd,5,8);
-    cout<<"The index of 8 is "<<index;
+    int index=binarySearch(arrOdd,5,7);
+    
+    cout<<"The index of 7 is "<<index;
     return 0;
 }
