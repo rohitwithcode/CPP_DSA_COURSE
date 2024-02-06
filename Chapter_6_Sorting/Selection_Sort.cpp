1b9654a81933c2bbe49e14e650077e520341a591
rohitwithcode <radharani7253@gmail.com>
2024-02-06 07:35:11 +0530

    Selection Sort file added

--- Chapter_6_Sorting/Selection_Sort.cpp
+++ Chapter_6_Sorting/Selection_Sort.cpp
@@ -1,33 +1,41 @@
 #include<iostream>
 using namespace std;
 
-void selectionSort(int arr[],int size)
+void print(int arr[],int size)
 {
-    for (int i = 0; i < size-1; i++)
-    {
-        int Minimum=i;
-        for (int  j = i+1; j < size; j++)
-        {
-            if (arr[Minimum] > arr[j])
-            {
-                Minimum=j;
-            }
-            
-        }
-        int temp=arr[i];
-        arr[i]=arr[Minimum];
-        arr[Minimum]=temp;
-    }
-    for (int i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
     {
         cout<<arr[i]<<" ";
     }
     cout<<endl;
 }
+
+void selectionSort(int arr[],int size)
+{
+    for (int i = 0; i < size-1; i++)
+    {
+        int minimumIndex=i;
+        for (int  j = i+1; j < size; j++)
+        {
+            if (arr[j] < arr[minimumIndex])
+            {
+                minimumIndex=j;
+            }
+            
+        }
+        int temp=arr[i];
+        arr[i]=arr[minimumIndex];
+        arr[minimumIndex]=temp;
+    }
+}
 int main()
 {
     int arr[]={64,12,34,25,11};
-    cout<<"Your Array after select sort "<<endl;
+    cout<<"Your Array before selection sort "<<endl;
+    print(arr,5);
+
+    cout<<"Your Array after selection sort "<<endl;
     selectionSort(arr,5);
+    print(arr,5);
     return 0;
 }
