ffbb7accb985b2830b3fbe4443a1f9169d2b732c
rohitwithcode <radharani7253@gmail.com>
2024-02-26 11:54:01 +0530

    Pointers intro file added

---
+++ Chapter_9_Pointers/Pointers_intro.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n=3;
+    cout<<"Value of n is "<<n<<endl;
+
+   // Address of operator - &
+
+   cout<<"The address of n is "<<&n<<endl;
+
+   // value at operator 
+
+   int *pointer=&n;
+
+   cout<<"Address of n is : "<<pointer<<endl;
+   cout<<"The value of n by dereference operator : "<<*pointer<<endl;
+
+    double d=45.444;
+    double *ptr=&d;
+
+    cout<<"Address of d is : "<<ptr<<endl;
+    cout<<"The value of d by dereference operator : "<<*ptr<<endl;
+
+    cout<<"The size of the Integer is : "<<sizeof(n)<<endl;
+    cout<<"The size of the pointer is : "<<sizeof(pointer)<<endl;
+
+    cout<<"The size of the Double is : "<<sizeof(d)<<endl;
+    cout<<"The size of the ptr is : "<<sizeof(ptr)<<endl;
+
+    return 0;
+}
